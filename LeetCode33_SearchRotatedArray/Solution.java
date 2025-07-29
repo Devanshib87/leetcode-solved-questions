9fbd2c4867adb804515962f876774a75bff195a7
Devanshi <acouanon19@gmail.com>
2025-07-29 15:37:57 +0530

    Added LeetCode 33 - Search in Rotated Sorted Array solution

---
+++ LeetCode33_SearchRotatedArray/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int search(int[] nums, int target) {
+        return search(nums,target,0,nums.length-1); 
+    }
+
+    public int search(int[] nums,int target,int si,int ei){
+        int mid = si +(ei-si)/2;
+        if(si>ei){
+            return -1;
+        }
+        if(nums[mid]==target){
+            return mid;
+        }
+        //case on line 1
+        if(nums[si]<=nums[mid]){
+            if(nums[si]<=target && target<=nums[mid]){
+                return search(nums,target,si,mid-1);
+            }
+            else{
+                return search(nums,target,mid+1,ei);
+            }
+        }
+        //case on line 2
+        else{
+            if(nums[mid]<=target && target<=nums[ei]){
+                return search(nums,target,mid+1,ei);
+            }
+            else{
+                return search(nums,target,si,mid-1);
+            }
+        }
+    }
+}
